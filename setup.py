d1b0df30ad298f7ccf4c7416d59d8589a9ef767e
Varun Suresh <fab.varun@gmail.com>
2017-06-25 14:29:25 -0500

    Basic package setup

---
+++ setup.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import (
+    setup,
+    find_packages)
+
+runtime_packages = [
+    'flask == 0.10.1',
+    'numpy==1.12.1',
+    'matplotlib==1.5.2',
+    'profilehooks==1.9.0',
+    'pandas==0.20.2',
+    'scipy==0.19.0'
+]
+
+setup(
+    name='approximate-nn-clustering',
+    version='0.0.1',
+    description='A modified version of rank order clustering',
+    author='Varun Suresh',
+    author_email='fab.varun@gmail.com',
+    license='MIT',
+    packages=find_packages(),
+    install_requires=runtime_packages,
+    )
