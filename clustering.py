7d24dbc49b81fc09880a13f1745e843df1c816ff
Varun Suresh <fab.varun@gmail.com>
2017-07-06 20:20:02 -0500

    Corrected version, remove repeating nodes

--- clustering.py
+++ clustering.py
@@ -35,7 +35,7 @@ def create_neighbor_lookup(nearest_neighbors):
 
 
 # @profile
-def calculate_symmetric_dist(nearest_neighbors, nn_lookup, row_no):
+def calculate_symmetric_dist_row(nearest_neighbors, nn_lookup, row_no):
     """
     This function calculates the symmetric distances for one row in the
     matrix.
@@ -62,30 +62,37 @@ def calculate_symmetric_dist(nearest_neighbors, nn_lookup, row_no):
     return dist_row
 
 
-def aro_clustering(app_nearest_neighbors, thresh):
-    '''
-    Approximate rank-order clustering. Takes in the nearest neighbors matrix
-    and outputs clusters - list of lists.
-    '''
+def calculate_symmetric_dist(app_nearest_neighbors):
+    """
+    This function calculates the symmetric distance matrix.
+    """
     dist_calc_time = time()
     nn_lookup = create_neighbor_lookup(app_nearest_neighbors)
     d = np.zeros(app_nearest_neighbors.shape)
     for row_no in range(app_nearest_neighbors.shape[0]):
-        d[row_no, :] = calculate_symmetric_dist(app_nearest_neighbors,
-                                                nn_lookup, row_no)
+        d[row_no, :] = calculate_symmetric_dist_row(app_nearest_neighbors,
+                                                    nn_lookup, row_no)
     d_time = time()-dist_calc_time
     print 'Distance calculation time : {}'.format(d_time)
+    return d
+
+
+def aro_clustering(app_nearest_neighbors, distance_matrix, thresh):
+    '''
+    Approximate rank-order clustering. Takes in the nearest neighbors matrix
+    and outputs clusters - list of lists.
+    '''
     # Clustering :
     clusters = []
     # Start with the first face :
-    nodes = set(list(np.arange(0, app_nearest_neighbors.shape[0])))
+    nodes = set(list(np.arange(0, distance_matrix.shape[0])))
     # print 'Nodes initial : {}'.format(nodes)
     tc = time()
     plausible_neighbors = create_plausible_neighbor_lookup(
                                                             app_nearest_neighbors,
-                                                            d,
+                                                            distance_matrix,
                                                             thresh)
-    print 'Time to create plausible_neighbors lookup : {}'.format(time()-tc)
+    # print 'Time to create plausible_neighbors lookup : {}'.format(time()-tc)
     ctime = time()
     while nodes:
         # Get a node :
@@ -102,6 +109,7 @@ def aro_clustering(app_nearest_neighbors, thresh):
             n = queue.pop(0)
             neighbors = plausible_neighbors[n]
             # Remove neighbors we've already visited :
+            neighbors = nodes.intersection(neighbors)
             neighbors.difference_update(group)
 
             # Remove nodes from the global set :
@@ -115,7 +123,7 @@ def aro_clustering(app_nearest_neighbors, thresh):
         # Add the group to the list of groups :
         clusters.append(group)
 
-    print 'Clustering Time : {}'.format(time()-ctime)
+    # print 'Clustering Time : {}'.format(time()-ctime)
     return clusters
 
 
@@ -129,24 +137,34 @@ def create_plausible_neighbor_lookup(app_nearest_neighbors,
     n_vectors = app_nearest_neighbors.shape[0]
     plausible_neighbors = {}
     for i in range(n_vectors):
-        min_dist = np.min(distance_matrix[i, 1:])
-        if min_dist <= thresh:
-            nn_indices = np.where(distance_matrix[i, :] == min_dist)
-            old_nn_row = app_nearest_neighbors[i, :]
-            plausible_neighbors[i] = set(list(old_nn_row[nn_indices]))
-        else:
-            plausible_neighbors[i] = set([])
+        plausible_neighbors[i] = set(list(app_nearest_neighbors[i,
+                                     np.where(
+                                            distance_matrix[i, :] <= thresh)]
+                                             [0]))
+        # min_dist = np.min(distance_matrix[i, 1:])
+        # if min_dist <= thresh:
+        #     nn_indices = np.where(distance_matrix[i, :] == min_dist)
+        #     old_nn_row = app_nearest_neighbors[i, :]
+        #     plausible_neighbors[i] = set(list(old_nn_row[nn_indices]))
+        # else:
+        #     plausible_neighbors[i] = set([])
     return plausible_neighbors
 
 
-def cluster(descriptor_matrix, n_neighbors=20, thresh=2):
+def cluster(descriptor_matrix, n_neighbors=20, thresh=[2]):
     """
     Master function. Takes the descriptor matrix and returns clusters.
     n_neighbors are the number of nearest neighbors considered and thresh
     is the clustering distance threshold
     """
     app_nearest_neighbors, dists = build_index(descriptor_matrix, n_neighbors)
-    clusters = aro_clustering(app_nearest_neighbors, thresh)
+    distance_matrix = calculate_symmetric_dist(app_nearest_neighbors)
+    # print distance_matrix
+    clusters = []
+    for th in thresh:
+        clusters_th = aro_clustering(app_nearest_neighbors, distance_matrix, th)
+        print 'N Clusters: {}, thresh: {}'.format(len(clusters_th), th)
+        clusters.append({'clusters': clusters_th, 'threshold': th})
     return clusters
 
 
